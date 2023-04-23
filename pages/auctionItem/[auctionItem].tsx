8d69c61fe60315ad487bed5cac2dc1387c4420f6
loarsaw <amanahmed1385@gmail.com>
2023-04-23 19:01:20 +0530

    some minor changes

--- pages/auctionItem/[auctionItem].tsx
+++ pages/auctionItem/[auctionItem].tsx
@@ -11,9 +11,10 @@ const Item = (props: Props) => {
   const [objValue, setObjValue] = useState<any>();
   const [inputValue, setValue] = useState<any>();
   const [user, setUser] = useState<any>();
+  const [sentReq, setReq] = useState(false);
   useEffect(() => {
     getData();
-  }, [pushToAuction]);
+  }, [sentReq]);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user: any) => {
@@ -206,7 +207,9 @@ const Item = (props: Props) => {
                   />
                 </div>
                 <button
-                  onClick={() => pushToAuction()}
+                  onClick={() => {
+                    pushToAuction(), setReq(!sentReq);
+                  }}
                   className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded"
                 >
                   Button
